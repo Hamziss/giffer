3fa9ddb7e4a1ca535de16a578c1c1547af0de5af
Hamza Chebbah <hamzachebbah9999@gmail.com>
2023-07-06 23:00:56 +0100

    added state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { Button } from "@geist-ui/core"
+import Smile from "@geist-ui/icons/smile"
+import GifPicker from "gif-picker-react"
+import { useState } from "react"
 import "./App.css"
 import Post from "./components/Post"
 import Profile from "./components/Profile"
-
 export default function App() {
+	const [isOpen, setIsOpen] = useState(false)
+	const [selected, setSelected] = useState(null)
 	const posts = [
 		{
 			id: 1,
@@ -30,7 +35,59 @@ export default function App() {
 				<Profile user={user} />
 			</div>
 			<div className="second-div">
-				<div></div>
+				<div
+					style={{
+						display: "flex",
+					}}
+				>
+					<div>
+						<div
+							style={{
+								position: "relative",
+								display: "flex",
+								alignItems: "center",
+								width: "100%",
+								justifyContent: "space-between",
+							}}
+						>
+							<div className="profilepic">
+								<img src="/random.jpeg" alt="" />
+							</div>
+							<div style={{ marginLeft: "30px" }}>
+								{isOpen && (
+									<div
+										style={{ position: "absolute", top: "54px", right: "72px" }}
+									>
+										<GifPicker
+											onGifClick={setSelected}
+											contentFilter="high"
+											tenorApiKey={"AIzaSyAEHmYxHg4PL0qHK_WtHkOtzSKOFJJB0Ps"}
+										/>
+									</div>
+								)}
+								<Button
+									onClick={() => setIsOpen(!isOpen)}
+									mr={0.5}
+									iconRight={<Smile />}
+									ml={"auto"}
+									auto
+									px={0.6}
+								/>
+							</div>
+							<Button auto>Post</Button>{" "}
+						</div>
+					</div>
+
+					{selected && (
+						<>
+							<img
+								src={selected.url}
+								className="gif-preview"
+								alt="Selected GIF"
+							/>
+						</>
+					)}
+				</div>
 				{posts.map((post) => (
 					<Post key={post.id} post={post} />
 				))}
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
+import { GeistProvider } from "@geist-ui/core"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<App />
+		<GeistProvider>
+			<App />
+		</GeistProvider>
 	</React.StrictMode>,
 )
